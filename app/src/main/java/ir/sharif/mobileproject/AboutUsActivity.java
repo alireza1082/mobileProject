902a238f2466795f0318b04d662411f3bf5ffb4c
alireza <rezaali1387@gmail.com>
2021-07-27 04:16:38 +0430

    added back button to AboutUsActivity

--- app/src/main/java/ir/sharif/mobileproject/AboutUsActivity.java
+++ app/src/main/java/ir/sharif/mobileproject/AboutUsActivity.java
@@ -1,8 +1,10 @@
 package ir.sharif.mobileproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class AboutUsActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class AboutUsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
