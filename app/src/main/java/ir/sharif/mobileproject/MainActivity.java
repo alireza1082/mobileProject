f8a08eadab08139d19988b6e926b3ba206857ebb
alireza <rezaali1387@gmail.com>
2021-07-26 23:23:10 +0430

    added listener to about us button

--- app/src/main/java/ir/sharif/mobileproject/MainActivity.java
+++ app/src/main/java/ir/sharif/mobileproject/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewbinding.ViewBinding;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -44,6 +45,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void setAboutButton(View view){
+        Intent intent = new Intent(this , AboutUsActivity.class);
+        startActivity(intent);
     }
 
 }
