88bb9e8c4b102e471eb82d21c11600ced0642a7f
alireza <rezaali1387@gmail.com>
2021-07-27 01:52:08 +0430

    added tutorial activity

--- app/src/main/java/ir/sharif/mobileproject/MainActivity.java
+++ app/src/main/java/ir/sharif/mobileproject/MainActivity.java
@@ -44,6 +44,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void setTutorialButton(View view){
+        Intent intent = new Intent(this , TutorialActivity.class);
+        startActivity(intent);
     }
 
     public void setAboutButton(View view){
