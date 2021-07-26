2df81a2d50a8391d2a6516ed5f3a8a4241b8a5e6
salehos <salehos763@gmail.com>
2021-07-27 01:27:21 +0430

    Add activity_date.xml and date_activity.java to files and add them to AndroidManifest.xml and edit mainActivity button

---
+++ app/src/main/java/ir/sharif/mobileproject/DateActivity.java
@@ -0,0 +1,23 @@
+package ir.sharif.mobileproject;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewbinding.ViewBinding;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+public class DateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_date);
+
+
+    }
+}
--- app/src/main/java/ir/sharif/mobileproject/MainActivity.java
+++ app/src/main/java/ir/sharif/mobileproject/MainActivity.java
@@ -13,17 +13,17 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button createButton = findViewById(R.id.createButton);
-    private Button checkButton = findViewById(R.id.checkButton);
-    private Button editButton = findViewById(R.id.editButton);
-    private Button tutorialButton = findViewById(R.id.tutorialButton);
-    private Button aboutButton = findViewById(R.id.aboutButton);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button createButton = findViewById(R.id.createButton);
+        Button checkButton = findViewById(R.id.checkButton);
+        Button editButton = findViewById(R.id.editButton);
+        Button tutorialButton = findViewById(R.id.tutorialButton);
+        Button aboutButton = findViewById(R.id.aboutButton);
         createButton.setOnClickListener(this::setCreateButton);
         checkButton.setOnClickListener(this::setCheckButton);
         editButton.setOnClickListener(this::setEditButton);
@@ -33,6 +33,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void setCreateButton(View view) {
+        Intent intent = new Intent(this , DateActivity.class);
+        startActivity(intent);
     }
 
     public void setCheckButton(View view) {
