3582040f1e003e50b9a47a1958a8ad5948656739
alireza <rezaali1387@gmail.com>
2021-07-27 02:17:34 +0430

    added CheckActivity buttons and listeners to project

--- app/src/main/java/ir/sharif/mobileproject/CheckActivity.java
+++ app/src/main/java/ir/sharif/mobileproject/CheckActivity.java
@@ -2,7 +2,10 @@ package ir.sharif.mobileproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CheckActivity extends AppCompatActivity {
 
@@ -10,5 +13,32 @@ public class CheckActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_check);
+
+        Button checkByNameButton = findViewById(R.id.checkByNameButton);
+        Button checkByIDButton = findViewById(R.id.checkByIDButton);
+        Button checkByTimeButton = findViewById(R.id.checkByTimeButton);
+        checkByNameButton.setOnClickListener(this::setCheckByNameButton);
+        checkByIDButton.setOnClickListener(this::setCheckByIDButton);
+        checkByTimeButton.setOnClickListener(this::setCheckByTimeButton);
+
     }
+
+    public void setCheckByNameButton(View view){
+        Intent intent = new Intent(this , SearchActivity.class);
+        intent.putExtra("value" , "name");
+        startActivity(intent);
+    }
+
+    public void setCheckByIDButton(View view){
+        Intent intent = new Intent(this , SearchActivity.class);
+        intent.putExtra("value" , "ID");
+        startActivity(intent);
+    }
+
+    public void setCheckByTimeButton(View view){
+        Intent intent = new Intent(this , SearchActivity.class);
+        intent.putExtra("value" , "time");
+        startActivity(intent);
+    }
+
 }
