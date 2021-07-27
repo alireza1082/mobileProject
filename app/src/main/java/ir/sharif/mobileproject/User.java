c8336de4f3dd565a9159251fd7ecad3a742be7ca
Salehos <Salehos763@gmail.com>
2021-07-27 07:31:03 +0430

    Add databaseHandler

---
+++ app/src/main/java/ir/sharif/mobileproject/User.java
@@ -0,0 +1,27 @@
+package ir.sharif.mobileproject;
+
+public class User {
+    private int id;
+    private String name;
+    private int cost;
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getCost() {
+        return cost;
+    }
+    public void setCost(String cost) {
+        this.cost = cost;
+    }
+
+}
