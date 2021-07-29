fe5f75c588407aecda2098e514f6f64f58963200
alireza <rezaali1387@gmail.com>
2021-07-29 19:23:10 +0430

    set clickListener to edit button

--- app/src/main/java/ir/sharif/mobileproject/editDateActivity.java
+++ app/src/main/java/ir/sharif/mobileproject/editDateActivity.java
@@ -2,13 +2,25 @@ package ir.sharif.mobileproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class editDateActivity extends AppCompatActivity {
+
+    Button edit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_date);
+
+        edit = findViewById(R.id.editButton);
+        edit.setOnClickListener(this::setEditButton);
+
+    }
+
+    public void setEditButton(View view) {
     }
 }
